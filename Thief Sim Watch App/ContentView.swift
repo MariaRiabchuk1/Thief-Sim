3b1fe1de7e04a20485b542fb7767e95b5c0371c7
MariaRiabchuk <riabchuk41@gmail.com>
2026-04-13 16:45:05 +0300

    Initial Commit

---
+++ Thief Sim Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thief Sim Watch App
+//
+//  Created by Marichka on 13.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thief Sim Watch App/Thief_SimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thief_SimApp.swift
+//  Thief Sim Watch App
+//
+//  Created by Marichka on 13.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Thief_Sim_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
